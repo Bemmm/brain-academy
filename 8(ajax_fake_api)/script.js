cf35263cdecba412469ae2717f4bfe6c672c7f33
Bogdan <bembenok@gmail.com>
2017-03-20 21:43:27 +0200

    add

--- 8(ajax_fake_api)/script.js
+++ 8(ajax_fake_api)/script.js
@@ -1,8 +1,13 @@
+$(document).ready(function(){
+	var token = localStorage.getItem('loggedUser');
+	alert(token);
+})
 var config = {
 	origin: 'https://reqres.in',
 	createUser: '/api/users',
 	loginUser: '/api/login'
 };
+
 $('#signUpForm').submit(function(event){
 	event.preventDefault();
 	var userObject = createObject(this);
@@ -15,23 +20,28 @@ $('#signInForm').submit(function(event){
 	sendData(userObject, config.loginUser);
 });
 
-
 function createObject(form){
 	var userModel = {};
-	// this = form
 	$(form).find('input').each(function(i, element){
 	   userModel[element.id] = element.value;
 	});
 	return userModel;
 }
-
-
+/**
+ * sends data to the server, for logining or registration
+ * @param  {Object} user  is generated in function createObject (19)
+ * @param  {String} route global variable, declared at first string
+ * @return {Object}    	  response from the server
+ */
 function sendData(user, route){
 	$.ajax({
 	    url: config.origin + route,
 	    type: "POST",
 	    data: user,
 	    success: function(response){
+	    	if(response.token){
+	    		localStorage.setItem('loggedUser', response.token);
+	    	};
 	        console.log(response);
 	    },
 	    error: function(error){
